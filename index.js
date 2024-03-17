0aaf5bdc639d278a47c24b107cc4123d67c844bf
Kyle <kyleyee522@gmail.com>
2024-03-17 14:23:56 -0700

    created user prompts

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const fs = require('fs');
+const inquirer = require('inquirer');
 
-function write(shape) {
-	fs.writeFile('./examples/example.svg', shape, (err) => {
-		err ? console.error(err) : console.log('Log created!');
-	});
-}
+inquirer.prompt([
+	{
+		type: 'input',
+		message: 'Enter text for the logo (Must not be more than 3 characters): ',
+		name: 'text',
+	},
+	{
+		type: 'input',
+		message: 'Enter a text color: ',
+		name: 'textColor',
+	},
+	{
+		type: 'list',
+		message: 'Select a shape for the logo: ',
+		name: 'shape',
+		choices: ['circle', 'square', 'triangle'],
+	},
+	{
+		type: 'input',
+		message: 'Enter a shape color: ',
+		name: 'shapeColor',
+	},
+]);
 
-const test = `<svg>
-			<circle cx="150" cy="100" r="80" x="0" y="0" width="300" height="200" fill=${setShapeColor}/>
-		</svg>`;
+// const test = `<svg>
+// 			<circle cx="150" cy="100" r="80" x="0" y="0" width="300" height="200" fill=${setShapeColor}/>
+// 		</svg>`;
 
 // write(test);
